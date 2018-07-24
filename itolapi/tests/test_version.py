6737e219b7735ca66ba4f76a230ec69aea588856
Albert Wang <git@albertyw.com>
2018-07-23 23:22:10 -0700

    Add test for __version__

---
+++ itolapi/tests/test_version.py
@@ -0,0 +1,8 @@
+import unittest
+
+from itolapi import __version__
+
+class TestVersion(unittest.TestCase):
+    def test_version(self):
+        self.assertTrue(__version__.__version__)
+        self.assertTrue(__version__.VERSION)
