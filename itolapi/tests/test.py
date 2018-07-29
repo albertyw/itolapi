9e8cc8649282f74bffd6e35162dd1e02f5c6c479
Albert Wang <git@albertyw.com>
2018-07-28 20:23:12 -0700

    Add integration test

---
+++ itolapi/tests/test.py
@@ -0,0 +1,25 @@
+import os
+import tempfile
+import unittest
+
+from itolapi import Itol
+
+
+class TestIntegration(unittest.TestCase):
+    def test_upload_download(self):
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        examples_dir = os.path.join(current_dir, '..', '..', 'examples')
+
+        test = Itol.Itol()
+        tree = os.path.join(examples_dir, 'tree_of_life.tree.txt')
+        test.add_variable('treeFile', tree)
+        dataset = os.path.join(examples_dir, 'colors_tol.txt')
+        test.add_variable('dataset1File', dataset)
+        upload_status = test.upload()
+
+        self.assertTrue(upload_status)
+
+        exporter = test.get_itol_export()
+        exporter.set_export_param_value('format', 'pdf')
+        with tempfile.NamedTemporaryFile() as temp:
+            exporter.export(temp.name)
